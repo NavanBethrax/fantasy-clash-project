2824d8596a41e5eff7dd820c15c8d0b175b1ed0a
NavanBethrax <syskiller1337@hotmail.com>
2018-02-06 23:09:46 +0100

    Second commit

---
+++ Fantasy Clash Project Debug/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports Fantasy_Clash_Project
+
+Public Class Form1
+
+    Dim engine As New Fantasy_Clash_Project.Main
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
